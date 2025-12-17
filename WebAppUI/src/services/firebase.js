7ff560032701d6a45e44c7b2fb6c40521b6f8227
yasrizalhakim <88261589+yasrizalhakim@users.noreply.github.com>
2025-12-17 09:37:41 +0800

    firebase details
    Replaced sensitive Firebase configuration details with placeholders.

--- WebAppUI/src/services/firebase.js
+++ WebAppUI/src/services/firebase.js
@@ -7,14 +7,14 @@ import { getDatabase } from 'firebase/database';
 // Your web app's Firebase configuration
 // Replace these with your actual Firebase project details
 const firebaseConfig = {
-  apiKey: "AIzaSyCg2kINGybYB0Nn55xGtmJBxBekIh0MFgA",
-  authDomain: "siseoa1.firebaseapp.com",
-  databaseURL: "https://siseoa1-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "siseoa1",
-  storageBucket: "siseoa1.firebasestorage.app",
-  messagingSenderId: "166548898974",
-  appId: "1:166548898974:web:f6f3d36631cddac54e678f",
-  measurementId: "G-5ENVPCLK00"
+  apiKey: "*******",
+  authDomain: "******",
+  databaseURL: "*******",
+  projectId: "*******",
+  storageBucket: "*********",
+  messagingSenderId: "*********",
+  appId: "********",
+  measurementId: "*******"
 };
 
 // Initialize Firebase
